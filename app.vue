66660dd7cf67c3f32b37517430eea7b10bb42442
SaveliyShutov <savelijsutov@gmail.com>
2024-07-16 18:08:56 +0400

    move init from app to default layout

--- app.vue
+++ app.vue
@@ -1,26 +1,3 @@
-<script setup lang="ts">
-useHead({
-  title: 'Plant Miner',
-})
-
-const userStore = useAuth()
-
-let tgId = ref(Number(window.Telegram.WebApp.id))
-let username = ref(window.Telegram.WebApp.username)
-let is_bot = ref(window.Telegram.WebApp.is_bot)
-
-declare global {
-  interface Window { Telegram: any; }
-}
-
-// let tgId = 885129018
-// let username = 'SaveliyShutov'
-// let is_bot = false
-onBeforeMount(async () => {
-  await userStore.login(tgId.value, username.value, is_bot.value)
-})
-
-</script>
 <template>
   <div>
     <NuxtLayout>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+useHead({
+    title: 'Plant Miner',
+    script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }]
+})
+
+declare global {
+    interface Window { Telegram: any; }
+}
+
+const userStore = useAuth()
+const tg = window.Telegram.WebApp
+
+let tgId = ref(Number(tg.id))
+let username = ref(tg.username)
+let is_bot = ref(tg.is_bot)
+
+
+// let tgId = 885129018
+// let username = 'SaveliyShutov'
+// let is_bot = false
+
+onBeforeMount(async () => {
+    tg.expand()
+    await userStore.login(tgId.value, username.value, is_bot.value)
+})
+
+</script>
 <template>
     <div>
         <UContainer>
