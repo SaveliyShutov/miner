98608d616c83020b7c154b839388bd2daf0caa4c
SaveliyShutov <savelijsutov@gmail.com>
2024-07-26 17:26:18 +0400

    fix tg try 5

--- app.vue
+++ app.vue
@@ -9,20 +9,20 @@ declare global {
 }
 
 const userStore = useAuth()
-const tg = window.Telegram.WebApp.initDataUnsafe.user
+let tg = ref(window.Telegram.WebApp.initDataUnsafe.user)
 
 // let tgId = 885129018
 // let username = 'SaveliyShutov'
 // let is_bot = false
 
 
-watch(tg, (val) => {
+watch(tg.value, (val) => {
   setApp()
 })
 
 let setApp = async () => {
-  await userStore.login(Number(tg.id), tg.username, tg.is_bot)
-  await userStore.test(tg)
+  await userStore.login(Number(tg.value.id), tg.value.username, tg.value.is_bot)
+  await userStore.test(tg.value)
 }
 
 // onMounted(async () => {
